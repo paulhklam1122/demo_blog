48748c02e928df0dbdd3a9ffe916a4d95e2d7021
Paul Lam <paulhklam1122@gmail.com>
2018-03-26 00:14:16 -0700

    post CRUD done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,54 @@
 class PostsController < ApplicationController
   def index
-    
+    @posts = Post.all.order('created_at DESC')
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new post_params
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    find_post
+  end
+
+  def edit
+    find_post
+  end
+
+  def update
+    find_post
+
+    if @post.update post_params
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    find_post
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def find_post
+    @post = Post.find params[:id]
   end
 end
